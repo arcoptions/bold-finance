3d76ac762cb81a52685971fb84168d440907f815
arcoptions <abhiramnarla@gmail.com>
2026-06-07 13:27:36 +0530

    Update dashboard_view.py

--- views/dashboard_view.py
+++ views/dashboard_view.py
@@ -75,7 +75,7 @@ def render_dashboard_view():
     st.markdown("---")
     
     # ROW 1: Waterfall & Trend
-    col_b = st.column()
+    cola_a,col_b = st.columns(2)
     
     with col_b:
         st.subheader("Monthly Revenue vs. Expenses")
