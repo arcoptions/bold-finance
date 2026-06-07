9bbc2aa7ee9202b0a66af6370eee165a49d5eee9
arcoptions <abhiramnarla@gmail.com>
2026-06-07 17:04:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from core.ui import inject_custom_css, render_header
 from views.expense_view import render_expense_view
 from views.recon_view import render_recon_view
 from views.dashboard_view import render_dashboard_view
+from views.invoice_view import render_invoice_view # Import the new view
 
 # 1. Page Configuration
 st.set_page_config(page_title="B&I Financial ERP", layout="wide", initial_sidebar_state="collapsed")
@@ -10,7 +11,8 @@ inject_custom_css()
 render_header()
 
 # 2. Main Horizontal Navigation
-tab_log, tab_recon, tab_dash = st.tabs(["Log Expense", "Reconciliation", "Dashboard"])
+# Added 'Invoice' tab here
+tab_log, tab_recon, tab_dash, tab_invoice = st.tabs(["Log Expense", "Reconciliation", "Dashboard", "Invoices"])
 
 # 3. Route to Views
 with tab_log:
@@ -21,3 +23,6 @@ with tab_recon:
 
 with tab_dash:
     render_dashboard_view()
+
+with tab_invoice:
+    render_invoice_view() # Route to the new function
